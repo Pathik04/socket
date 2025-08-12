61921060de67082dfa21b72d4c208ba5a4180aa4
Pathik04 <pathikrit2004@gmail.com>
2025-08-13 00:12:15 +0530

    commit

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ app.get('/', (req, res) => res.sendFile(join(__dirname, 'index.html')))
 //Define an connection event handler
 io.on('connection', (client_one) => {  // disconnecting a specific client fromthe server
     console.log("User connected to (server) ✅");
+
+//Emit a message event to the client
+// client_one.emit('message' , "Server is sending the data to the client")  // the emit method allows to => send < the data 
+
+client_one.on("new message", (message)=>{
+    console.log(message);
+})
+
     client_one.on('disconnect', () => {
         console.log('User Disconnected from (Server)❌');
     })
